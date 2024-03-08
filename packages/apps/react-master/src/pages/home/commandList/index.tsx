2539d2eb1f75971db4e10c86b891437f18e83db2
黄智强 <huangzq@dustess.com>
2024-03-08 21:38:15 +0800

    feat(react-master):  无限滚动

--- packages/apps/react-master/src/pages/home/commandList/index.tsx
+++ packages/apps/react-master/src/pages/home/commandList/index.tsx
@@ -1,4 +1,10 @@
-import React, { FC, MouseEventHandler, useState } from "react";
+import React, {
+	FC,
+	MouseEventHandler,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
 import { mockList } from "./mock";
 
 type Props = {};
@@ -76,12 +82,48 @@ const CommandData: FC<ICommandItem> = ({ item }) => {
 	);
 };
 
+const fetchList = () =>
+	new Promise<Array<any>>((resolve) => {
+		setTimeout(() => {
+			resolve(mockList.slice(5, 10));
+		}, 500);
+	});
+
 export default function CommandList({}: Props) {
+	const [list, setList] = useState(mockList.slice(0, 5));
+
+	const scrollRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		let intersectionObserver: IntersectionObserver | undefined =
+			new IntersectionObserver((entries) => {
+				// 这个函数执行时，拿不到最新的 list
+				const isIntersecting = entries[0]?.isIntersecting;
+
+				if (isIntersecting) {
+					// 加载更多数据
+					fetchList().then((res: Array<any>) => {
+						setList((list) => [...list, ...res]);
+
+						// setList([...list, ...res]); 这样写，list 不会更新
+					});
+				}
+			});
+
+		scrollRef.current && intersectionObserver.observe(scrollRef.current);
+
+		return () => {
+			scrollRef.current && intersectionObserver!.unobserve(scrollRef.current);
+			intersectionObserver = void 0;
+		};
+	}, []);
+
 	return (
 		<div className=" flex flex-col border-t">
-			{mockList.map((item, idx) => (
+			{list.map((item, idx) => (
 				<CommandData key={item.id + idx} item={item} />
 			))}
+			<div ref={scrollRef}>loading......</div>
 		</div>
 	);
 }
