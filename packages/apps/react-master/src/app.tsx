4f8c3914943a322eb4bc8e560d89bcd6acf77420
黄智强 <huangzq@dustess.com>
2024-03-08 12:28:32 +0800

    feat(react-master):  改造app.tsx

--- packages/apps/react-master/src/app.tsx
+++ packages/apps/react-master/src/app.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import "./app.css";
-import styles from "./app2.module.less";
+import { HashRouter, useRoutes } from "react-router-dom";
+import { router } from "./router";
 
-type Props = {};
+type Props = {
+	name?: string;
+};
+
+// 放在 App 外面，防止每次渲染都重新生成
+const Routers = () => useRoutes(router);
 
 export default function App({}: Props) {
 	return (
-		<div className={`hello ${styles.greenBGColor} flex justify-center`}>
-			App
-		</div>
+		<HashRouter>
+			<Routers />
+		</HashRouter>
 	);
 }
