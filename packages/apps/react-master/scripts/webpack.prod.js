d66aa318362b5bc05b4a61185724a832dd2f337a
黄智强 <huangzq@dustess.com>
2024-03-08 10:26:50 +0800

    chore(react-master): 老代码修改（可忽略）

--- packages/apps/react-master/scripts/webpack.prod.js
+++ packages/apps/react-master/scripts/webpack.prod.js
@@ -4,5 +4,7 @@ const baseConfig = require("./webpack.base");
 
 // 基于 webpack 的官方 merge 方法，将 baseConfig 和 prodConfig 合并
 module.exports = merge(baseConfig(), {
+	// 生产环境配置
+
 	mode: "production",
 });
