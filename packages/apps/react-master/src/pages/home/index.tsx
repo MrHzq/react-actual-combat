ff44bf093823d53935f3b1f5acd022d300c9a814
黄智强 <huangzq@dustess.com>
2024-03-08 17:42:33 +0800

    feat(react-master):  完善首页代码

--- packages/apps/react-master/src/pages/home/index.tsx
+++ packages/apps/react-master/src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
 import Navigation from "../../components/navigation";
+import Card from "../../components/card";
+import Tabs from "./tabs";
 
 type Props = {};
 
@@ -8,7 +9,16 @@ export default function Home({}: Props) {
 	return (
 		<div>
 			<Navigation />
-			<Outlet />
+			<div className=" mx-auto max-w-6xl flex my-2 px-20">
+				<Card className=" w-2/3">
+					<Tabs />
+				</Card>
+				<div className=" flex-1 w-1/3">
+					<Card className=" w-full">创作中心</Card>
+					<Card className=" w-full">推荐关注</Card>
+					<Card className=" w-full">其他功能</Card>
+				</div>
+			</div>
 		</div>
 	);
 }
