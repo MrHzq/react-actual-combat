84209409ea68c68e64b8ee9fd40fabaf8e3f8ff7
黄智强 <huangzq@dustess.com>
2024-03-09 20:34:54 +0800

    feat(react-x-components): src/button/index.tsx写入代码 2

--- packages/components/react-x-components/src/button/index.tsx
+++ packages/components/react-x-components/src/button/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 type Props = {
 	children: React.ReactNode;
+	onClick?: () => void;
 };
 
-export default function Button({ children }: Props) {
+export default function Button({ children, onClick }: Props) {
 	return (
-		<button className=" w-16 h-8 mx-4 text-sm bg-blue-500 text-white flex justify-center items-center rounded-full hover:bg-blue-800 transition-all">
+		<button
+			onClick={onClick}
+			className=" w-16 h-8 mx-4 text-sm bg-blue-500 text-white flex justify-center items-center rounded-full hover:bg-blue-800 transition-all"
+		>
 			{children}
 		</button>
 	);
