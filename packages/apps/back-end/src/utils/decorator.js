2a83404dc6dcf7b1b485ccc24a6d3f54fc9e0f2f
黄智强 <huangzq@dustess.com>
2024-03-10 15:41:10 +0800

    feat(back-end): src/utils/decorator.js写如下代码

---
+++ packages/apps/back-end/src/utils/decorator.js
@@ -0,0 +1,10 @@
+export const RequestMethod = {
+	GET: "get",
+	POST: "post",
+	PUT: "put",
+	DELETE: "delete",
+};
+
+export function Controller(perfix = "") {}
+
+export function RequestMapping(method = "", url = "") {}
