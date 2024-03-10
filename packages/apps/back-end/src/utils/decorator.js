fc09932f48d2dd2117cdf869ba74c723452e1181
黄智强 <huangzq@dustess.com>
2024-03-10 16:29:47 +0800

    feat(back-end): 完善src/utils/decorator.js代码

--- packages/apps/back-end/src/utils/decorator.js
+++ packages/apps/back-end/src/utils/decorator.js
@@ -5,6 +5,25 @@ export const RequestMethod = {
 	DELETE: "delete",
 };
 
-export function Controller(perfix = "") {}
+export const controllers = [];
 
-export function RequestMapping(method = "", url = "") {}
+export function Controller(prefix = "") {
+	return function (constructor) {
+		constructor.prefix = prefix;
+	};
+}
+
+export function RequestMapping(method = "", url = "") {
+	return function (target, propertyKey, decriptor) {
+		let path = url || `/${propertyKey}`;
+
+		const item = {
+			method,
+			path,
+			handler: decriptor.value, // 函数自身
+			constructor: target.constructor, // 构造函数
+		};
+
+		controllers.push(item);
+	};
+}
