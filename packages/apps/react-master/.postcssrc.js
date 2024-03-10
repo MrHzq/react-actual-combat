688bc0816f7e053c41decfb8a643795ec0319bf2
黄智强 <huangzq@dustess.com>
2024-03-10 13:59:57 +0800

    chore(react-master themePlugin 插件封装): 更改.postcssrc.js文件，加入对应插件

--- packages/apps/react-master/.postcssrc.js
+++ packages/apps/react-master/.postcssrc.js
@@ -1,6 +1,10 @@
+const { themeGroup, defaultTheme } = require("./themeGroup");
 module.exports = {
 	plugins: [
 		"autoprefixer", // 自动添加浏览器前缀
 		"tailwindcss",
+		"postcss-nested",
+		"postcss-nesting",
+		require("./themePlugin")({ themeGroup, defaultTheme }),
 	],
 };
