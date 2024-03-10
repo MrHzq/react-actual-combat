59ec66738f696de36645a81a1b5ddb10091a2659
黄智强 <huangzq@dustess.com>
2024-03-10 16:30:34 +0800

    feat(back-end): 更改book.js

--- packages/apps/back-end/src/controllers/book.js
+++ packages/apps/back-end/src/controllers/book.js
@@ -1,13 +1,9 @@
-const {
-	Controller,
-	RequestMapping,
-	RequestMethod,
-} = require("../utils/decorator");
+import { Controller, RequestMapping, RequestMethod } from "../utils/decorator";
 
 @Controller("/book")
 export default class BookController {
 	@RequestMapping(RequestMethod.GET, "/all")
 	async getAll(ctx) {
-		ctx.body = [];
+		ctx.body = ["1", "2"];
 	}
 }
