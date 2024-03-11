4c4a3fb25a79a80ca7796cbe2685ffd34a5a436f
黄智强 <huangzq@dustess.com>
2024-03-11 08:46:57 +0800

    feat(react-master): src/utils/lib/apis.ts编码

--- packages/apps/react-master/src/utils/lib/apis.ts
+++ packages/apps/react-master/src/utils/lib/apis.ts
@@ -1,5 +1,35 @@
 import { BaseTrack, UserTrackData } from "./track";
 
+// 性能上报
+export class Performance {
+	static readonly timing = window.performance && window.performance.timing;
+
+	static init() {
+		if (!this.timing) {
+			console.warn("performance is not support");
+		}
+
+		window.addEventListener("load", () => {
+			const data = this.getTiming();
+
+			new BaseTrack().track(data);
+		});
+	}
+	static getTiming(): UserTrackData {
+		const timing = this.timing;
+
+		return {
+			type: "performance",
+			data: {
+				loadTime: timing.loadEventEnd - timing.navigationStart,
+				domReadyTime: timing.domComplete - timing.domLoading,
+				readyTime: timing.domContentLoadedEventEnd - timing.navigationStart,
+				requestTime: timing.responseEnd - timing.requestStart,
+			},
+		};
+	}
+}
+// 主动上报
 const t = new BaseTrack();
 export const sendLog = <T>(data: T) => {
 	t.track(data as T & UserTrackData);
